043d4d37de6a86c61cfc4d05a9bd643b229dd207
Gina Fedder <ginafedder@yahoo.com>
2018-09-25 17:15:35 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,7 @@ i = 0
 block_return_values = []
   while i < collection.length 
     block_return_values << yield(collection[i])
-    i = i + l
+    i = i + 1
   end
   if block_return_values.include?(false)
       false
